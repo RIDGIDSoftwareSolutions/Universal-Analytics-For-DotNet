5ad02b05f6100b0ece07234eaf7e9a3d7375e693
jakejgordon <jakejgordon@gmail.com>
2017-03-02 07:53:47 -0500

    Removed unused references

--- Tests/UniversalAnalyticsHttpWrapper.Tests/GoogleDataSenderTests.cs
+++ Tests/UniversalAnalyticsHttpWrapper.Tests/GoogleDataSenderTests.cs
@@ -1,13 +1,4 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using UniversalAnalyticsHttpWrapper;
-
-namespace UniversalAnalyticsHttpWrapper.Tests
+﻿namespace UniversalAnalyticsHttpWrapper.Tests
 {
     //[TestFixture]
     //public class GoogleDataSenderTests
